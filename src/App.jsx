a83d791bd877ba6042dd528c412f078f2c711ea2
nguyenphamducanh366 <nguyenphamducanh366@gmail.com>
2024-12-07 12:09:07 +0700

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,33 @@ import Add from "./Add";
 
 
 const App = () => {
-
+const[user,setUser]=useState(null);
+const PrivateRoute=({children,user})=>{
+  if(!user){
+    return <Navigate to={'/signin'}></Navigate>
+  }
+  if(user !== 1){
+  }
+  return children;
+}
 return(
-  <div> <h1>Test</h1></div>
-
+<Routes>
+ <Route path="/products" element={
+  <PrivateRoute user={user}><List/></PrivateRoute>
+ }></Route>
+  <Route path="/products/add" element={
+  <PrivateRoute user={user}><Add/></PrivateRoute>
+ }></Route>
+  <Route path="/products/:id/edit" element={
+  <PrivateRoute user={user}><Update/></PrivateRoute>
+ }></Route>
+  <Route path="/signin" element={
+  <Signin setUser={setUser} />
+ }></Route>
+  <Route path="/signup" element={
+  <Signup/>
+ }></Route>
+</Routes>
 )
 };
 
